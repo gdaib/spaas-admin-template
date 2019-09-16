885d8bbb55085aa464456eb574f14575fb87f97a
GaryHjy <782316959@qq.com>
2019-09-16 19:52:49 +0800

    feat: 🎸 删除proxy.config无用的接口代理
    删除proxy.config无用的接口代理

--- config/proxy.config.js
+++ config/proxy.config.js
@@ -5,16 +5,10 @@ const mockServer = 'http://39.98.50.163:3000/mock/995';
 
 module.exports = {
   mock: {
-    '/deepexi-dashboard': mockServer,
-    '/spaas-enterprise-contact': mockServer,
-    '/spaas-console-api': mockServer,
-    '/spaas-application-center': apiServer,
-    '/deepexi-cloud': apiServer,
+    '/spaas-application-center': mockServer,
+    '/deepexi-cloud': mockServer,
   },
   dev: {
-    '/deepexi-dashboard': apiServer,
-    '/spaas-enterprise-contact': apiServer,
-    '/spaas-console-api': apiServer,
     '/spaas-application-center': apiServer,
     '/deepexi-cloud': apiServer,
   },
